d41fd768e526e70768288ed4f9cd73807950bf0d
xido <xidoweb@gmail.com>
2021-11-08 12:25:11 +0900

    js - 비교연산자

--- c_code/b_step_3/js/src/b_03_operation.js
+++ c_code/b_step_3/js/src/b_03_operation.js
@@ -75,9 +75,94 @@ nFn(++n);
 nFn(++n);
 nFn();
 
+// ------------------------------
+console.clear();
+var  i = 5;
+console.log(i); // 5
 
+i = i + 1;
+console.log(i); // 6
 
+i += 1;
+console.log(i); // 7
+
+++i;
+console.log(i); // 8
+
+i++;
+console.log(i); // 9
+
+console.log(i++); // 9  : 변수 i의 값을 먼저 도출 후 연산1을 더한다.
+console.log(i++); // 10
+console.log(i); // 11
+console.log(++i); // 12 : 변수 i의 값을 먼저 연산 후에 도출
+console.log(++i); // 13
+console.log(i); // 13
+
+// 위 전치/후치의 계산차이는 실무에서는 큰 차이는 없다!
+// 우리의 일상과 비교한다면, 한국식나이계산과 미국식 나이계산차이로 이해
+// 실무작업에서는 정확한 값을 도출 후 작업을 진행하는 것을 권장하기에
+
+nFn();
+
+n = n + 1;
+n += 1;
+n++;
+++n;
+nFn('-------------');
+
+n = n - 1;
+n -= 1;
+nFn();
+n--;
+--n;
+nFn();
+console.clear();
+// -------------------------------------------------------------------
+// 비교연산자 =======================================
+n = 20;
+nFn( i ); // n값과 i값 비교
+
+var compare = n === i; // === 같은가? n값과i의 값이 같이 않으므로 false
+console.log( compare );
+
+compare = n !== i; // !== 같지않은가? true
+console.log( compare );
+
+compare = n <= i;
+console.log( compare );
+
+compare = n >= i;
+console.log( compare );
+
+// or(||) , and(&&)
+// ||, && 비교연산자는 좌변과 우변을 비교하여, true/false를 찾아내는 기법
+// 한쪽이 완전한 충족을하면 다른 쪽은 비교하지 않는다!
+// 단, 하나의 비교연산자( |, & )사용시 결과는 0,1로 도출
+// 팁, 조금이라도 빠른 연산을 위해
+// ||은 true가 나오기쉽거나 우선이되는 내용을 좌변에 배치
+// &&는 falser가 나오기 쉽거나 우선이 되는 내용을 좌변에 배치
+
+var compare2 = true || false; // || 둘중 하나이상 참이면 true
+console.log( compare2 );
+
+compare2 = false && false;  // && 둘중 하나라도 거짓이면 false
+console.log( compare2 );
+
+compare3 = true & true; 
+console.log('|, & 하나짜리로 비교', compare3 );
+
+// !의 의미는 부정(반대)
+console.log('체크1:',  !!false ); 
+console.log('체크2:',  0 , !!0);
+
+// ---------------------------------
+var checkFn = function(data){
+  var d = data || '??';
+  return console.log( d );
+};
+
+checkFn(10);
+checkFn();
 
 // 삼항연산자 =======================================
-
-// 비교연산자 =======================================
