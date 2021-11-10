f4de43ea1eab54f2591b2fc6962c9ceb7e5f678e
xido <xidoweb@gmail.com>
2021-11-10 12:05:18 +0900

    javascript - switch 응용1

--- c_code/b_step_03/js/src/b_05_js_condition.js
+++ c_code/b_step_03/js/src/b_05_js_condition.js
@@ -41,7 +41,7 @@ var atmFn = function (num) {
       atmFn(2);
       break;
     case 2:
-      data.push('현금 확인');
+      data.push('통장 확인');
       break;
     case 3:
       data.push('타계좌 송금');
@@ -58,3 +58,91 @@ var atmFn = function (num) {
 };
 atmFn(1);
 console.log(data);
+
+// ---------------------------------------------
+console.clear();
+
+//a: 편의점에서 물건을 살경우
+// 1. 물건을 고른다.
+// 2. 카드를 낸다.
+// 3. 카드 결재를한다.
+// 4. 물건과, 카드를 돌려받는다.
+// 5. 내역을 확인한다.
+
+//b: 편의점에서 공병을 팔경우
+// 1. 물건을 낸다
+// 2. 카드를 낸다.
+// 3. 카드 입급을 받는다.
+// 4. 카드를 돌려받는다.
+// 5. 내역을 확인한다.
+
+//c: 편의점에서 물건을 환불
+// 1. 물건을 건넨다.
+// 2. 카드를 낸다.
+// 3. 카드 입금을 받는다(취소)
+// 4. 카드를 돌려받는다
+// 5. 내역을 확인한다.
+
+//d: 편의점 결재가 안될때
+// 1. 물건을 고른다.
+// 2. 카드를 낸다.
+// 3. 결재 에러발생
+// 4. 카드 돌려받는다.
+// 5. 내역을 확인한다.
+
+var storeList = [];
+var i = 0;
+
+var storeFn = function (selector, user) {
+  var userCheck = {}; //{name:'사용자', content: []};
+  var userStep = [];
+  // ------------------------------
+  switch (selector) {
+    case 'a':// 물건을 살경우
+      userStep.push('1. 물건을 고른다.');
+      userStep.push('2. 카드를 낸다.');
+      userStep.push('3. 카드 결재를한다.');
+      userStep.push('4. 물건과, 카드를 돌려받는다.');
+      userStep.push('5. 내역을 확인한다.');
+      break;
+
+    case 'b':// 공병을 팔경우
+      userStep.push('1. 물건을 낸다');
+      userStep.push('2. 카드를 낸다.');
+      userStep.push('3. 카드 입급을 받는다.');
+      userStep.push('4. 카드를 돌려받는다.');
+      userStep.push('5. 내역을 확인한다.');
+      break;
+
+    case 'c':// 환불
+      userStep.push('1. 물건을 건넨다.');
+      userStep.push('2. 카드를 낸다.');
+      userStep.push('3. 카드 입금을 받는다(취소처리).');
+      userStep.push('4. 카드를 돌려받는다');
+      userStep.push('5. 내역을 확인한다.');
+      break;
+
+    case 'd':// 결재가 안될때
+      userStep.push('1. 물건을 고른다.');
+      userStep.push('2. 카드를 낸다.');
+      userStep.push('3. 결재 에러발생');
+      userStep.push('4. 카드 돌려받는다.');
+      userStep.push('5. 내역을 확인한다.');
+      break;
+
+    default:
+      userStep.push('...');
+  }
+  // ------------------------------
+  i += 1;
+  userCheck.name = user || 'noNameUser_' + i;
+  userCheck.content = userStep;
+  storeList.push(userCheck);
+  return userCheck;
+};// storeFn();
+
+console.log(storeFn('a'));
+console.log(storeFn('b', 'user2'));
+console.log(storeFn('c'));
+console.log(storeFn('d', 'user4'));
+console.log(storeList);
