4925b45ed2223936806236610ba5fb91ba62967a
xido <xidoweb@gmail.com>
2021-11-15 18:14:07 +0900

    깊은복사 얕은 복사

--- c_code/b_step_03/js/src/b_09_js_copy.js
+++ c_code/b_step_03/js/src/b_09_js_copy.js
@@ -12,6 +12,8 @@ var cloneColor = [];
 // ['emerald green', 'coral','hot pink','city blue', 'brown gray','yellow','sky']; -> favoritColor
 // ['emerald green', 'coral','hot pink','city blue', 'brown gray','yellow','deep orange']; -> cloneColor
 
+
+// 방법 1-----------------------------------
 /*
   cloneColor.push(favoritColor[0]);
   cloneColor.push(favoritColor[1]);
@@ -28,9 +30,74 @@ var cloneColor = [];
 
 var i = 0;
 var len = favoritColor.length;
-for(; i < len; i+=1){
+// 방법2 -------------------------
+/*
+for(; i <= len; i+=1){
   cloneColor.push(favoritColor[i]);
 }
 favoritColor.push('sky');
 cloneColor.push('deep orange');
-console.log(favoritColor, cloneColor);
+*/
+
+// 방법3 -----------------------
+/* 
+for(; i <= len; i+=1){
+  if(i !== len){
+    cloneColor.push(favoritColor[i]);
+  }else{
+    favoritColor.push('sky');
+    cloneColor.push('deep orange');
+  }
+}
+*/
+// 방법 4 ----------------------------------
+
+favoritColor.forEach(function(data, index){
+  // cloneColor[index] = favoritColor[index];
+  // cloneColor.push(data);
+  cloneColor[index] = data;
+});
+favoritColor.push('sky');
+cloneColor.push('deep orange');
+
+// console.log(favoritColor, cloneColor);
+
+
+// ==============================================================
+var pc = {
+  'dell'    : '프리시전',
+  'hp'      : 'z시리즈',
+  'apple'   : 'mac book',
+  'samsung' : 'galaxy book'
+}; 
+var copyPc = pc;
+// pc.lg = 'gear';
+pc['lg'] = 'gear';
+// console.log( copyPc );
+
+var clonePc = {};
+// 결과
+// { 'dell' : '프리시전', 'hp' : 'z시리즈', 'apple' : 'mac book', 'samsung' : 'galaxy book', 'lg':'gear',asus:'gen book' } -> pc
+// { 'dell' : '프리시전', 'hp' : 'z시리즈', 'apple' : 'mac book', 'samsung' : 'galaxy book', 'lg':'gear',lenovo: 'think pad' } -> clonePc
+
+// 객체 방법1 --------------------
+/*
+clonePc['dell'] = '프리시전';
+clonePc['hp'] = 'z시리즈';
+clonePc['apple'] = 'mac book';
+clonePc['samsung'] = 'galaxy book';
+*/
+
+for(var data in pc){
+  // console.log( data );
+  clonePc[data] = pc[data];
+}
+
+pc['asus'] = 'gen book';
+clonePc['lenovo'] = 'think pad';
+
+console.log(pc);
+console.log(clonePc);
+
+// 참조 주소를 복사하는 것 - 얕은 복사(shallow copy)
+// 참조 주소에 있는 값 자체를 복사 - 깊은 복사(deep copy)
