48ce7f1b9130f552c4cfd878050a30d01d22c727
xido <xidoweb@gmail.com>
2021-11-16 12:24:29 +0900

    if, switch

--- c_code/b_step_03/js/src/b_10_js_remind_01-1.js
+++ c_code/b_step_03/js/src/b_10_js_remind_01-1.js
@@ -58,3 +58,53 @@ var fn = function(a){
 
 var fnCon = fn(arr[3]);
 console.log( fnCon );
+
+// ---------------------------------------
+
+// if(){}else{}
+// switch(){}
+// for(){}  , for(in){} , [].forEach()
+// function(){}
+
+var launch = '돈까스';
+if(launch === '짜장면'){
+  console.log('짜장면먹으러 갑시다!');
+}else if(launch === '볶음밥'){
+  console.log('볶음밥먹으러 갑시다!');
+}else if(launch === '삼겹살'){
+  console.log('삼겹살먹으러 갑시다!');
+}else{
+  console.log(launch+' 먹겠습니다.');
+}
+
+var switchN = 5;
+var data;
+switch(switchN){
+  case 1:
+    data = '마우스 왼버튼을 클릭하였습니다.';
+    break;
+  case 2:
+    data = '마우스 오른버튼을 클릭하였습니다.';
+    break;
+  case 3:
+    data = '마우스 휠버튼을 클릭하였습니다.';
+    break;
+  case 4:
+    data = '마우스 측면버튼을 클릭하였습니다.';
+    break;
+  default:
+    data = '마우스 기타 다른버튼을 클릭하였습니다.';
+    break;
+}
+console.log( data );
+
+var arr2 = [1,2,4,3,5,6,8,3,5,9,10,20,50,26];
+// for(최초; 비교; 증감){}
+for(var j=0; j<10; j++){ // 10번 반복 수행
+  console.log( 'j:', j, '-', arr2[j] );
+}
+
+// [].forEach(function(각요소하나씩, 순서){})
+arr2.forEach(function(arg, idx){
+  console.log(idx,':', arg, arr2[idx] );
+});
