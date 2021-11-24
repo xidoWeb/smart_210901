2e3fcd36e975646041eafb45f51a30fbebd562d1
xido <xidoweb@gmail.com>
2021-11-24 18:23:43 +0900

    기능일부 잘못된부분 수정

--- c_code/b_step_03/js/src/c_02_dom_make_navi_02.js
+++ c_code/b_step_03/js/src/c_02_dom_make_navi_02.js
@@ -18,18 +18,20 @@ var subMenu = document.querySelector('.sub_menu');
 var subMenuStyle = getComputedStyle(subMenu);
 // console.log( subMenuStyle.display );
 
+
 var displayStateFn = function(selector){
   // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
     return selector.style.display || getComputedStyle(selector).display;
     /*
     var displayState;
       if(!!selector.style.display){
-        displayState = getComputedStyle(selector).display;
-      }else {
         displayState = selector.style.display
+      }else {
+        displayState = getComputedStyle(selector).display;
       }
     */
 };
+
 // 이벤트 ----------------------------------------------------
 dropBtn.addEventListener('click', function(event){
   event.preventDefault();
@@ -38,3 +40,10 @@ dropBtn.addEventListener('click', function(event){
   console.log( state );
   (state === 'none') ? subMenu.style.display = 'block' :  subMenu.style.display = 'none';
 });
+// ----------------------------------------------------------------
+console.clear();
+
+var ck = subMenu.style.display; // 0 , false, null, undefined, ''
+console.log( ck  );
+console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
