6d71d7a736e5d00583a03cb196d5203ec26e7df8
xido <xidoweb@gmail.com>
2021-11-29 16:10:26 +0900

    js 선택자에대한 내용1

--- c_code/b_step_03/js/src/c_07_dom_selector_01.js
+++ c_code/b_step_03/js/src/c_07_dom_selector_01.js
@@ -2,7 +2,7 @@
 
 // #wrap {}
 var wrap = document.getElementById('wrap');
-console.log( wrap );
+// console.log( wrap );
 // wrap.style.width = '90%';
 // wrap.style.margin = 'auto';
 // wrap.style.backgroundColor = '#eee';
@@ -16,7 +16,20 @@ h1.style = 'width:100px; height:30px; background-color:#dfd;';
 var h1Link = h1.children;
 h1Link[0].style = 'padding:0.5rem; color:#05f;';
 h1Link[1].classList.add('blind');
-console.log( h1Link );
+// console.log( h1Link );
+
+var navArea = wrap.getElementsByClassName('nav_area'); 
+// var navArea2 = wrap.getElementsByTagName('nav')[0].className === 'nav_area';
+// console.log( navArea2 );
+navArea[0].style = 'width:100%';
+
+var navH2 = navArea[0].children[0];
+navH2.classList.add('blind');
+var navUl = navArea[0].children[1];
+navUl.style = 'display:flex; '
+
+var navLi = navUl.children;
+console.log( navLi );
 
 // ------------------------------------------------------------------
 var headBox = document.querySelector('#headBox');
@@ -26,6 +39,15 @@ var headH1 = headBox.querySelector('h1');
 headH1.style.borderRadius = '5px';
 headH1.style.fontSize = '1.2rem';
 
+var h1Insert = headH1.childNodes;
+// console.log( h1Insert );
+
+var navArea3 = headBox.querySelector('.nav_area');
+navArea3.style.height = '30px';
+navArea3.style.backgroundColor = '#eee';
+
+var navli2 = navArea3.querySelectorAll('li');
+console.log( navli2 );
 
 // ---------------------------------------------------------------------------
 // HTML collection : 옛날방식의 선택자
