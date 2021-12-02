0b729440ce8a1d25bc2a851423c098916424bbc7
xido <xidoweb@gmail.com>
2021-12-02 16:16:14 +0900

    [add/modify] : javascript - 객체data dom처리 이미지 삽입 처리

--- c_code/b_step_03/js/src/c_11_dom_insert_data.js
+++ c_code/b_step_03/js/src/c_11_dom_insert_data.js
@@ -1,31 +1,55 @@
 // c_11_dom_insert_data.js
 // 별도 자료 ----------------------------
 var data = [
-  {title:'title'      , 
-  contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  link:'http://naver.com' }, 
+  {
+    title:'title'      , 
+    contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    link:'http://naver.com', 
+    bgImg:'../multi/img/bottle.jpg',
+    bgNarr:'병이미지에 대한 간략한 설명'
+  }, 
 
-  {title:'spring'     , 
-  contents:'Dolores laborum expedita eum maxime dolore porro ipsam.' ,
-  link:'http://csswinner.com' },
+  {
+    title:'spring'     , 
+    contents:'Dolores laborum expedita eum maxime dolore porro ipsam.' ,
+    link:'http://csswinner.com' ,
+    bgImg:'../multi/img/cookie.jpg',
+    bgNarr:'cookie 이미지에 설명 작성'
+  },
 
-  {title:'javascript' , 
-  contents:'Quod, impedit?' ,
-  link:'http://gdweb.co.kr' },
+  {
+    title:'javascript' , 
+    contents:'Quod, impedit?' ,
+    link:'http://gdweb.co.kr' 
+  },
 
-  {title:'winter'     , 
-  contents:'Rem enim tempore maxime voluptas facilis id ad commodi rerum unde quis.' ,
-  link:'http://naver.net' },
+  {
+    title:'winter'     , 
+    contents:'Rem enim tempore maxime voluptas facilis id ad commodi quis.' ,
+    link:'http://naver.net',
+    bgImg:'../multi/img/bottle.jpg',
+    bgNarr:'병이미지에 대한 간략한 설명'
+  },
   
-  {title:'summer'     , 
-  contents:'Lorem, ipsum dolor.' ,
-  link:'http://google.com' }, 
+  {
+    title:'summer'     , 
+    contents:'Lorem, ipsum dolor.' ,
+    link:'http://google.com',
+    bgImg:'../multi/img/juice.jpg',
+    bgNarr:'juice image 설명'
+  }, 
 
-  {title:'banana'     , 
-  contents:'Lorem ipsum dolor sit amet.' },
+  {
+    title:'banana'     , 
+    contents:'Lorem ipsum dolor sit amet.' 
+  },
   
-  {title:'group'      , 
-  contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.' }
+  {
+    title:'group'      , 
+    contents:'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    bgImg:'../multi/img/juice.jpg',
+    bgNarr:'juice 이미지에 대한 간략한 설명'
+  }
 ];
 // --------------------------------------
 // 시나리오: 
@@ -44,7 +68,7 @@ var makeLi;
 for(; i<len; i+=1){
   makeLi = document.createElement('li');
   // makeLi.innerHTML = (i + 1) + data[i].title + ' ' + '<br />' + data[i].contents + ' ';
-  makeLi.innerHTML = '<dl><dt>' + data[i].title + '</dt><dd><span>' + data[i].contents + '<span><br /><a href=\"' + data[i].link + '\" target=\"_blank\">자세히 보기</a></dd></dl>';
+  makeLi.innerHTML = '<dl><div class="bg_img" style="background-image:url(' + data[i].bgImg + ')"><span class="blind">' + data[i].bgNarr + '</span></div><dt>' + data[i].title + '</dt><dd><span>' + data[i].contents + '<span><br /><a href=\"' + data[i].link + '\" target=\"_blank\">자세히 보기</a></dd></dl>';
   cardUl.appendChild(makeLi);
 }
 // ---------------------------------------
