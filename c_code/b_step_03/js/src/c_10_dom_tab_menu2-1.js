83ba35471d55b83ef944c4c76cff653459c052c4
xido <xidoweb@gmail.com>
2021-12-02 10:55:38 +0900

    js - class이름 및 속성 추가

--- c_code/b_step_03/js/src/c_10_dom_tab_menu2-1.js
+++ c_code/b_step_03/js/src/c_10_dom_tab_menu2-1.js
@@ -13,13 +13,24 @@ var latestYear = yearPart[0];
 
 // 기능
   // 1. ul생성
-    var makeUl = document.createElement('hi');
-    var mkt = '안녕하세요!';
-    latestYear.append( mkt );// 문자도가능 
-    latestYear.appendChild( mkt );// 문자불가능
+    var makeUl = document.createElement('ul');
+    var makeLi = document.createElement('li');
+
+    makeUl.className = 'two'; // 기존내용지우고 대체
+    makeUl.classList.add('one');// 기존에 추가
+    makeUl.setAttribute('data-id','three'); // 대체, 속성 변경가능
+
+    latestYear.append( makeUl );
+    // latestYear.appendChild( mkt );// 문자불가능
+    var yearUl = latestYear.querySelector('ul');
+    yearUl.append(makeLi);
 
 
 // ------------------------------------------------------------------------------------
 // append 는 지정된 선택자 내부에 필요한 요소(내용)을 기존 내용에 추가로 덧붙이는 기능
 // appendChild 는 'Node'형식으로 구성된 내용만 담을수 있다(element).
 // createElement는 요소를 생성하는 기능 - 생성만 가능하며 1회용, 추가로 덧붙이기 위해서는 append, appendChild ..을 사용
+// 요소에 class속성을 추가하는 방법 
+  // 1. 선택자.classList.add('이름'), 
+  // 2. 선택자.className = '이름', 
+  // 3. 선택자.setAttribute('속성명','이름');
