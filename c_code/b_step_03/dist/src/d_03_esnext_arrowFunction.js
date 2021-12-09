2149d841d19b191957afe9db6e7777b90fed5a45
xido <erivery2@gmail.com>
2021-12-09 12:21:35 +0900

    [add] javascript(arrow function) :ab:  esnext - 화살표함수2

--- c_code/b_step_03/dist/src/d_03_esnext_arrowFunction.js
+++ c_code/b_step_03/dist/src/d_03_esnext_arrowFunction.js
@@ -1,4 +1,7 @@
 // d_03_esnext_arrowFunction.js
+// 화살표함수는 생성자 함수 
+// 또는 return처리할 값이 없는 함수를 제외하고 사용이 가능하다.
+
 
 // 함수표현식
 const fnBox = function(a){
@@ -8,8 +11,30 @@ fnBox(5);
 console.log( fnBox );
 // -----------------------
 const fnBox2 = a => a * 10;
-
 console.log( fnBox2 );
-// 즉시실행함수
+// -----------------------
 
-// 함수 선언식
+//함수 선언식
+function fnBox3 (a, b) {
+  const c = (a + b) * 100 + 'size';
+  return c;
+}
+
+let rel = fnBox3(5,10);
+console.log( rel );
+
+const fnBox4 = (a, b) => {
+  const c = (a + b) * 100 + 'size';
+  return c;
+}
+let rel2 = fnBox4(10,3);
+console.log( rel2 );
+
+// --------------------------------
+
+const arrNum = [1,2,4,6,8,9,20,40,56,75,88];
+// const minNum = arrNum.filter(function(part){
+//   return 40 >= part && part > 10 ;
+// });
+const minNum = arrNum.filter( part => 40 >= part && part > 10 );
+console.log( minNum );
--- c_code/b_step_03/js/src/d_03_esnext_arrowFunction.js
+++ c_code/b_step_03/js/src/d_03_esnext_arrowFunction.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // d_03_esnext_arrowFunction.js
+// 화살표함수는 생성자 함수 
+// 또는 return처리할 값이 없는 함수를 제외하고 사용이 가능하다.
 // 함수표현식
 var fnBox = function fnBox(a) {
   return a * 10;
@@ -13,5 +15,30 @@ var fnBox2 = function fnBox2(a) {
   return a * 10;
 };
 
-console.log(fnBox2); // 즉시실행함수
-// 함수 선언식
+console.log(fnBox2); // -----------------------
+//함수 선언식
+
+function fnBox3(a, b) {
+  var c = (a + b) * 100 + 'size';
+  return c;
+}
+
+var rel = fnBox3(5, 10);
+console.log(rel);
+
+var fnBox4 = function fnBox4(a, b) {
+  var c = (a + b) * 100 + 'size';
+  return c;
+};
+
+var rel2 = fnBox4(10, 3);
+console.log(rel2); // --------------------------------
+
+var arrNum = [1, 2, 4, 6, 8, 9, 20, 40, 56, 75, 88]; // const minNum = arrNum.filter(function(part){
+//   return 40 >= part && part > 10 ;
+// });
+
+var minNum = arrNum.filter(function (part) {
+  return 40 >= part && part > 10;
+});
+console.log(minNum);
