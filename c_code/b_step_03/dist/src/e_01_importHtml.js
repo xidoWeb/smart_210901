5fbfe971e8e0977163870e33cee4e39807edab68
xido <erivery2@gmail.com>
2021-12-14 15:38:39 +0900

    [add] 문서분리 :ab: html 문서 분리 후 처리

--- c_code/b_step_03/dist/src/e_01_importHtml.js
+++ c_code/b_step_03/dist/src/e_01_importHtml.js
@@ -4,19 +4,24 @@
 
 // 불러올 data 변수
 const headCode = '../temp/header.html';
+const slideArea = '../temp/slide_area.html';
 
 // 선택변수
 const wrap = document.querySelector('#wrap');
 
 const headBox = document.createElement('div');
-headBox.id = 'headBox';
-wrap.prepend(headBox);
-
+const viewBox = document.querySelector('#viewBox');
 
 fetch(headCode)
   .then( response => {
     return response.text();
   })
   .then((data)=>{
+    headBox.id = 'headBox';
+    wrap.prepend(headBox);
     headBox.innerHTML = data;
   });
+
+fetch(slideArea)
+  .then( response => response.text() )
+  .then( data => viewBox.innerHTML = data  );
--- c_code/b_step_03/dist/src/e_01_slide_area.js
+++ c_code/b_step_03/dist/src/e_01_slide_area.js
@@ -15,165 +15,167 @@
 
 // 2. 이전버튼을 클릭시 위 기능을 동일하게 수행
 // -------------------------------------
+window.addEventListener('load', function(){
 
-// 요소 선택
-const elSlide_01 = document.querySelector('.slide_01');
+  // 요소 선택
+  const elSlide_01 = document.querySelector('.slide_01');
 
-const elBtnDetail = elSlide_01.querySelector('.view_btn');
-const elNext = elBtnDetail.querySelector('.next');
-const elPrev = elBtnDetail.querySelector('.prev');
+  const elBtnDetail = elSlide_01.querySelector('.view_btn');
+  const elNext = elBtnDetail.querySelector('.next');
+  const elPrev = elBtnDetail.querySelector('.prev');
 
-const elViewContent = elSlide_01.querySelector('.view_content');
-const elViewConUl = elViewContent.querySelector('ul');
-const elViewAddv = elViewConUl.querySelectorAll('li');
+  const elViewContent = elSlide_01.querySelector('.view_content');
+  const elViewConUl = elViewContent.querySelector('ul');
+  const elViewAddv = elViewConUl.querySelectorAll('li');
 
-const elIndicatorDetail = elSlide_01.querySelector('.indicator ul');
-const elIndiLi = elIndicatorDetail.children;
-const elIndiList = Array.prototype.slice.call(elIndiLi);
+  const elIndicatorDetail = elSlide_01.querySelector('.indicator ul');
+  const elIndiLi = elIndicatorDetail.children;
+  const elIndiList = Array.prototype.slice.call(elIndiLi);
 
-// elAddLen
-const addLen = elViewAddv.length;
-const OPTION_CLASSNAME = 'on';
-let checkIndex = 0;
-// -----------------------------------------------------------
-// 함수
-const fnAddCountType1 = () => {
-  let i = checkIndex;
-  // 0 -> 1 -> 2 -> 3 -> 0
-  (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
-  elViewAddv[i].classList.remove(OPTION_CLASSNAME);
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-};
+  // elAddLen
+  const addLen = elViewAddv.length;
+  const OPTION_CLASSNAME = 'on';
+  let checkIndex = 0;
+  // -----------------------------------------------------------
+  // 함수
+  const fnAddCountType1 = () => {
+    let i = checkIndex;
+    // 0 -> 1 -> 2 -> 3 -> 0
+    (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
+    elViewAddv[i].classList.remove(OPTION_CLASSNAME);
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+  };
 
-const fnRemoveCountType1 = () => {
-  let i = checkIndex;
-  // 0 -> 3(length-1) -> 2 -> 1 -> 0
-  (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
-  elViewAddv[i].classList.remove(OPTION_CLASSNAME);
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-};
-// ----------------------------------------
-// 전체 갯수 중 선택한 순번을 제외한 나머지 형제를 선택
-const fnSiblings = (select, idx = checkIndex) => {
-  const otherArr = [];
-  select.forEach((element, index)=>{
-    //let check = element.classList.contains(OPTION_CLASSNAME);
-    // if(!check){ otherArr.push(element) }
-    if(idx !== index){ otherArr.push(element) }
-  })
-  return otherArr;
-};
-// console.log( fnSiblings() );
+  const fnRemoveCountType1 = () => {
+    let i = checkIndex;
+    // 0 -> 3(length-1) -> 2 -> 1 -> 0
+    (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
+    elViewAddv[i].classList.remove(OPTION_CLASSNAME);
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+  };
+  // ----------------------------------------
+  // 전체 갯수 중 선택한 순번을 제외한 나머지 형제를 선택
+  const fnSiblings = (select, idx = checkIndex) => {
+    const otherArr = [];
+    select.forEach((element, index)=>{
+      //let check = element.classList.contains(OPTION_CLASSNAME);
+      // if(!check){ otherArr.push(element) }
+      if(idx !== index){ otherArr.push(element) }
+    })
+    return otherArr;
+  };
+  // console.log( fnSiblings() );
 
-// 다음버튼클릭시 1씩 카운트업하면서, 선택순번이 아닌경우 on빼라
-const fnAddCountType2 = () => {
-  (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-  fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
-    el.classList.remove(OPTION_CLASSNAME);
-  });
-};
-const fnRemoveCountType2 = () => {
-  (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-  fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
-    el.classList.remove(OPTION_CLASSNAME);
-  });
-};
+  // 다음버튼클릭시 1씩 카운트업하면서, 선택순번이 아닌경우 on빼라
+  const fnAddCountType2 = () => {
+    (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+    fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
+      el.classList.remove(OPTION_CLASSNAME);
+    });
+  };
+  const fnRemoveCountType2 = () => {
+    (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+    fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
+      el.classList.remove(OPTION_CLASSNAME);
+    });
+  };
 
-const fnIndexCountType2 = () => {
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-  fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
-    el.classList.remove(OPTION_CLASSNAME);
-  });
-}
-// ----------------------------------------------------------
-const fnAddCountType3 = () => {
-  (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
-  elViewAddv.forEach( (el)=>{  el.classList.remove(OPTION_CLASSNAME); });
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-};
-
-const fnRemoveCountType3 = () => {
-  (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
-  elViewAddv.forEach( (el)=>{  el.classList.remove(OPTION_CLASSNAME); });
-  elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
-};
-// -----------------------------------------------------------
-// 이벤트 ++
-// 다음버튼 클릭
-elNext.addEventListener('click', (e) => {
-  e.preventDefault();
-  // fnAddCountType1();
-  // fnAddCountType2();
-  fnAddCountType3();
-});
-
-// 이전버튼 클릭
-elPrev.addEventListener('click', (e) => {
-  e.preventDefault();
-  // fnRemoveCountType1();
-  // fnRemoveCountType2();
-  fnRemoveCountType3();
-});
-
-// indicator 클릭하여 순서 파악
-elIndiList.forEach( (element, index) => {
-  let link = element.querySelector('a');
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    // console.log( index );
-    checkIndex = index;
-    fnIndexCountType2();
-  });
-});
-
-// ============================================================
-
-const fnT = function( i = '값 입력하세요~~!' ){  
-  // if(!i){ 
-  //   return '값 입력되지 않았음' 
-  // }else{
-  //   return i;
-  // }
-  
-  // return i || '값 입력되지 않았음';
-  
-  return i;  
-}
-
-console.log( fnT(12) );
-
-
-
-const arrayList = ['a', 'b','c','d'];
-// console.log(arrayList[0]);
-// console.log(arrayList[2]);
-
-// for(let i=0; i<4; i++){
-//   console.log( arrayList[i] );
-// }
-
-arrayList.forEach( (d,i) => {
-  // console.log( d );
-  console.log(arrayList[i]);
-});
-// forEach ( 배열의 각요소, 각요소의 순번 )
-
-const arrayList2 = ['a', 'b','c','d'];
-const otherArr = [];
-let selectI = 1;
-
-arrayList2.forEach( (d,i) => {
-  if(selectI !== i){
-    otherArr.push(d);
+  const fnIndexCountType2 = () => {
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+    fnSiblings(elViewAddv, checkIndex).forEach( (el) => {
+      el.classList.remove(OPTION_CLASSNAME);
+    });
   }
-});
+  // ----------------------------------------------------------
+  const fnAddCountType3 = () => {
+    (checkIndex < addLen -1) ? checkIndex += 1 : checkIndex = 0;
+    elViewAddv.forEach( (el)=>{  el.classList.remove(OPTION_CLASSNAME); });
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+  };
 
-const otherArr2 = arrayList.filter((el)=>{
-  return el !== arrayList2[selectI];
-});
-console.log( otherArr );
-console.log( otherArr2 );
+  const fnRemoveCountType3 = () => {
+    (checkIndex > 0) ? checkIndex -= 1 : checkIndex = addLen -1;
+    elViewAddv.forEach( (el)=>{  el.classList.remove(OPTION_CLASSNAME); });
+    elViewAddv[checkIndex].classList.add(OPTION_CLASSNAME);
+  };
+  // -----------------------------------------------------------
+  // 이벤트 ++
+  // 다음버튼 클릭
+  elNext.addEventListener('click', (e) => {
+    e.preventDefault();
+    // fnAddCountType1();
+    // fnAddCountType2();
+    fnAddCountType3();
+  });
+
+  // 이전버튼 클릭
+  elPrev.addEventListener('click', (e) => {
+    e.preventDefault();
+    // fnRemoveCountType1();
+    // fnRemoveCountType2();
+    fnRemoveCountType3();
+  });
+
+  // indicator 클릭하여 순서 파악
+  elIndiList.forEach( (element, index) => {
+    let link = element.querySelector('a');
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      // console.log( index );
+      checkIndex = index;
+      fnIndexCountType2();
+    });
+  });
+
+  // ============================================================
+
+  const fnT = function( i = '값 입력하세요~~!' ){  
+    // if(!i){ 
+    //   return '값 입력되지 않았음' 
+    // }else{
+    //   return i;
+    // }
+    
+    // return i || '값 입력되지 않았음';
+    
+    return i;  
+  }
+
+  console.log( fnT(12) );
+
+
+
+  const arrayList = ['a', 'b','c','d'];
+  // console.log(arrayList[0]);
+  // console.log(arrayList[2]);
+
+  // for(let i=0; i<4; i++){
+  //   console.log( arrayList[i] );
+  // }
+
+  arrayList.forEach( (d,i) => {
+    // console.log( d );
+    console.log(arrayList[i]);
+  });
+  // forEach ( 배열의 각요소, 각요소의 순번 )
+
+  const arrayList2 = ['a', 'b','c','d'];
+  const otherArr = [];
+  let selectI = 1;
+
+  arrayList2.forEach( (d,i) => {
+    if(selectI !== i){
+      otherArr.push(d);
+    }
+  });
+
+  const otherArr2 = arrayList.filter((el)=>{
+    return el !== arrayList2[selectI];
+  });
+  console.log( otherArr );
+  console.log( otherArr2 );
 
 // -----------------------------------------
+})
