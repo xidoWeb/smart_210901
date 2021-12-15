e36f94bc7015128ce1a9d4fe24a825c3a03ebe53
xido <erivery2@gmail.com>
2021-12-15 15:14:52 +0900

    [add] slide_area-fade :ab: slide-fade 처리 css설정 으로 일부 변경

--- c_code/b_step_03/dist/src/e_01_slide_area_fade.js
+++ c_code/b_step_03/dist/src/e_01_slide_area_fade.js
@@ -2,7 +2,7 @@
 // -------------------------------------
 // 시나리오
 // 다음버튼 클릭시 - 'fadeOut'/(fadeIn) 기능을 이용하여, 이미지 변경
-// 1. 현재 보이는 이미지(z-index:10) 에 버튼 클릭시 보이는 이미지 바로뒤 에 이미지가 나타나게 만들기
+// 1. 현재 보이는 이미지(z-index:100) 에 버튼 클릭시 보이는 이미지 바로뒤 에 이미지가 나타나게 만들기
 // 2. 현재 보이는 이미지를 투명도 1->0으로 변경 ( 100%, 50%, 0 )
 // 3. 투명도가 0이 되면 display:none으로 사라지게
 // 4. animation기법으로 처리되어 사라진 이미지 style(html에 새겨진) 제거
@@ -28,38 +28,45 @@ const viewLi  = viewCon.querySelectorAll('li');
 let [CLASS_CHECK, countIndex, timed, cssFn] = ['act', 0, 500, 'ease'];
 let beforeIndex = countIndex;
 const liLen = viewLi.length;
+let permission = true;
 // -------------------------------------------------------------
-viewLi[countIndex].classList.remove('on');
+// viewLi[countIndex].classList.remove('on');
+// viewLi[countIndex].classList.add(CLASS_CHECK);
+viewLi[countIndex].classList.replace('on', CLASS_CHECK);
 
 
 // -------------------------------------------------------------
 // 함수
-const fnInsertStyle = () => {
-  viewLi[beforeIndex].style.display    = 'block';
-  viewLi[beforeIndex].style.zIndex     = '100';
-  viewLi[beforeIndex].style.position   = 'absolute'; // css에서 작성
-  viewLi[beforeIndex].style.transition = `all ${timed}ms  ${cssFn}`; //css에서 or js
-};
+// const fnDelay = async ( t = timed ) => {
+//   const promise = new Promise(resolve => {
+//     setTimeout( ()=> resolve(permission = true), t)
+//   });
+//   return await promise;
+// };
+
 
 const fnOpacity = () => {
-  viewLi[countIndex].style.display = 'block';
-  viewLi[beforeIndex].style.opacity = 0;
-    // 수행2
-    setTimeout( () => {
-      viewLi[beforeIndex].removeAttribute('style'); // viewLi[beforeIndex].style = null;
-      beforeIndex = countIndex;
-      fnInsertStyle();
-    }, timed);
-};
-// 함수 기본 수행
-fnInsertStyle();
+    viewLi[countIndex].style.display = 'block'; // 바로 뒤에 나타나기
+
+    viewLi[beforeIndex].style.transition = `all ${timed}ms  ${cssFn}`; // 사라질기능 animation
+    viewLi[beforeIndex].style.opacity = 0;   // 투명도조절 + 위animation기능 처리
+    
+    setTimeout(()=>{
+      viewLi[beforeIndex].removeAttribute('style');  // js설정된 style제거 (animate 제거)
+      viewLi[beforeIndex].classList.remove(CLASS_CHECK); // .act 제거 (display:none)
+
+      viewLi[countIndex].classList.add(CLASS_CHECK); // 순번이 바뀐 요소 (.act 첨부)
+      beforeIndex = countIndex;  // 순서 기존변수, 변경변수값 통일
+    },timed);
+  }; // fnOpacity; 
 // -------------------------------------------------------------
 // 이벤트처리
 // elNext클릭시
   elNext.addEventListener('click', (e)=>{
     e.preventDefault();
     // 수행1
-    (countIndex < liLen-1) ? countIndex += 1 : countIndex = 0;
-    fnOpacity();
+      (countIndex < liLen-1) ? countIndex += 1 : countIndex = 0;
+      fnOpacity();
   });
+
 })()
