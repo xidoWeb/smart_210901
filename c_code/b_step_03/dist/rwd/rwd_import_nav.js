7ca6fe8bbf81eb83de20676ab751d811b18885cc
xido <erivery2@gmail.com>
2021-12-24 11:04:24 +0900

    [modify] :ambulance: 위치오류

--- c_code/b_step_03/dist/rwd/rwd_import_nav.js
+++ c_code/b_step_03/dist/rwd/rwd_import_nav.js
@@ -1,14 +1,15 @@
 // rwd_import_nav.js
 
 {
+  
+  const path = '../temp/';
+  const data = {
+    mob : path + 'mobile_rwd_nav.html',
+    laptop : path + 'laptop_rwd_nav.html'
+  }
+  const elNav = document.querySelector('#navBox');
 
-  const fnDeviceNav = (type)=>{
-    const path = '../temp/';
-    const data = {
-      mob : path + 'mobile_rwd_nav.html',
-      laptop : path + 'laptop_rwd_nav.html'
-    }
-    const elNav = document.querySelector('#navBox');
+  const fnDeviceNav = (type)=>{   
   
     fetch(type)
     .then(response => response.text())
