c270423b5b193d97671256e56e4c8c0bcdedc8e7
xido <erivery2@gmail.com>
2021-12-24 16:15:54 +0900

    [add] :ab: 반응형 구현을 위한 mediaMatches

--- c_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
+++ c_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
@@ -2,14 +2,13 @@
 
 // matchMedia를 이용하여 해당규격에 일치하는지(matches) 판단
 // window.matchMedia('css크기를 그대로 옮겨놓은 형태').matches 
-
+/*
 const wrap = document.querySelector('#wrap');
 const mobile = document.querySelector('.mobile');
 const pc = document.querySelector('.pc');
 mobile.remove();
 pc.remove();
 const size = 'screen and (max-width:768px)';
-// const size2 = 'screen and (min-width:320px) and (max-width:768px)';
 const mediaQuery = window.matchMedia(size);  
 // 지정규격과 체크해서 맞으면 true 아니면 false
 const fnCKMedia = ()=>{
@@ -29,15 +28,52 @@ fnCKMedia();
 mediaQuery.addEventListener('change', (e)=>{
   fnCKMedia();
 });
+*/
 
 // 이전버전에서는 무조건 브라우저의 크기가 변화할때마다(resize) 매번 확인해서 그 규격에 맞게 설정
 // matchMedia는 최초의 설정된 크기를 기준으로 해당내용이 변경(change)되면 바로 확인하여 설정
 
 
 // Question 어떻게 아래 규격을 확인할 것인가?
-const dataDevice = [
+const dataDevice = [ // dataDevice[n].size
   { type: 'mobile' , size : 599 }, // 600미만
   { type: 'tablet' , size : 767 }, // 600 ~ 767
   { type: 'laptop' , size : 1439 },// 768 ~ 1439
   { type: 'pc' }                   // 1440 ~     
 ];
+
+const mobile = window.matchMedia(`screen and (max-width:${dataDevice[0].size}px)`);
+const tablet = window.matchMedia(`screen and (min-width:${dataDevice[0].size + 1}px) and (max-width:${dataDevice[1].size}px)`);
+const laptop = window.matchMedia(`screen and (min-width:${dataDevice[1].size + 1}px) and (max-width:${dataDevice[2].size}px)`);
+const pc     = window.matchMedia(`screen and (min-width:${dataDevice[2].size + 1}px)`);
+
+const mediaArray = [mobile, tablet, laptop, pc];
+
+// mobile.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 mobile규격'); }
+// });
+// tablet.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 tablet규격'); }
+// });
+// laptop.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 laptop규격'); }
+// });
+// pc.addEventListener('change', e=>{
+//   if(e.matches){ console.log('현재 pc규격'); }
+// });
+
+const fnCKDevice = (device, index)=>{
+  if(device.matches){ console.log( dataDevice[index].type )}
+}
+
+mediaArray.forEach( (type,index) =>  {
+    // if(e.matches){ console.log( dataDevice[index].type )}
+    fnCKDevice(type, index);
+});
+
+mediaArray.forEach( (type,index) =>  {
+  type.addEventListener('change', (e) =>{
+    // if(e.matches){ console.log( dataDevice[index].type )}
+    fnCKDevice(e, index);
+  });
+});
