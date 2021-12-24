66cffdffdb05db562dc03618c3f755844e664ee9
xido <erivery2@gmail.com>
2021-12-24 15:08:22 +0900

    [add] responsive js :ab: matchMedia이용하여 기준크기변경체크

---
+++ c_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
@@ -0,0 +1,33 @@
+// d_12_rwd_matchMedia.js
+
+// matchMedia를 이용하여 해당규격에 일치하는지(matches) 판단
+// window.matchMedia('css크기를 그대로 옮겨놓은 형태').matches 
+
+const wrap = document.querySelector('#wrap');
+const mobile = document.querySelector('.mobile');
+const pc = document.querySelector('.pc');
+mobile.remove();
+pc.remove();
+const size = 'screen and (max-width:768px)';
+const mediaQuery = window.matchMedia(size);  
+// 지정규격과 체크해서 맞으면 true 아니면 false
+const fnCKMedia = ()=>{
+  if(mediaQuery.matches){
+    console.log('768보다 작은 크기');
+    wrap.append(mobile);
+    pc.remove();
+  }else{
+    console.log('768보다 큰 크기');
+    wrap.append(pc);
+    mobile.remove();
+  }
+};
+fnCKMedia();
+// ---------------------
+// 정해진크기규격(max-width:768px)에서 변화할때 
+mediaQuery.addEventListener('change', (e)=>{
+  fnCKMedia();
+});
+
+// 이전버전에서는 무조건 브라우저의 크기가 변화할때마다(resize) 매번 확인해서 그 규격에 맞게 설정
+// matchMedia는 최초의 설정된 크기를 기준으로 해당내용이 변경(change)되면 바로 확인하여 설정
