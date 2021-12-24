1253e3f4f908727d74e3cd1995ce04265ee3ee16
xido <erivery2@gmail.com>
2021-12-24 15:19:18 +0900

    [question] :ab: 규격체크

--- c_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
+++ c_code/b_step_03/dist/src/d_12_rwd_matchMedia.js
@@ -9,6 +9,7 @@ const pc = document.querySelector('.pc');
 mobile.remove();
 pc.remove();
 const size = 'screen and (max-width:768px)';
+// const size2 = 'screen and (min-width:320px) and (max-width:768px)';
 const mediaQuery = window.matchMedia(size);  
 // 지정규격과 체크해서 맞으면 true 아니면 false
 const fnCKMedia = ()=>{
@@ -31,3 +32,12 @@ mediaQuery.addEventListener('change', (e)=>{
 
 // 이전버전에서는 무조건 브라우저의 크기가 변화할때마다(resize) 매번 확인해서 그 규격에 맞게 설정
 // matchMedia는 최초의 설정된 크기를 기준으로 해당내용이 변경(change)되면 바로 확인하여 설정
+
+
+// Question 어떻게 아래 규격을 확인할 것인가?
+const dataDevice = [
+  { type: 'mobile' , size : 599 }, // 600미만
+  { type: 'tablet' , size : 767 }, // 600 ~ 767
+  { type: 'laptop' , size : 1439 },// 768 ~ 1439
+  { type: 'pc' }                   // 1440 ~     
+];
