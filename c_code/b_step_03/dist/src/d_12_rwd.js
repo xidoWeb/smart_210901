aeebd32079787ffc6e4befc7ef58f876c9e9cc1f
xido <erivery2@gmail.com>
2021-12-24 12:22:56 +0900

    [add] :ab: 반응형 수행시 기준크기값이 여러개일경우 처리

--- c_code/b_step_03/dist/src/d_12_rwd.js
+++ c_code/b_step_03/dist/src/d_12_rwd.js
@@ -7,24 +7,49 @@ const elPc = document.querySelector('.pc');
 
 // ----------------------------------------------
 const tab = 768; // 모든브라우저 사용가능
-const tabDevice = '(max-width:768px)'; // 구형X
+const laptop = 1440;
+
+const dataDevice = [
+  { type: 'mobile' , size : 599 },
+  { type: 'tablet' , size : 767 },
+  { type: 'laptop' , size : 1439 },
+  { type: 'pc' }
+];
+
+let CK_DEVICE;
+
+// const tabDevice = '(max-width:768px)'; // 구형X
 
 let winWidth = window.innerWidth; // 브라우저의 가로크기
-console.log( winWidth );
+// console.log( winWidth );
 const fnRwd = (deviceType)=>{
-  if(deviceType < tab){
-    elMobile.style.display = 'block';
-    elPc.style.display = 'none';
+  if(deviceType <= dataDevice[0].size){
+    CK_DEVICE = dataDevice[0].type;
+  }else if(deviceType > dataDevice[0].size && deviceType <= dataDevice[1].size){
+    CK_DEVICE = dataDevice[1].type;
+  }else if(deviceType > dataDevice[1].size && deviceType <= dataDevice[2].size){
+    CK_DEVICE = dataDevice[2].type;
   }else{
-    elMobile.style.display = 'none';
-    elPc.style.display = 'block';
+    CK_DEVICE = dataDevice[3].type;
   }
+// --------------------
+  switch(CK_DEVICE){
+    case dataDevice[0].type:
+    case dataDevice[1].type:
+      elMobile.style.display = 'block';
+      elPc.style.display = 'none';
+      break;
+    default:
+      elMobile.style.display = 'none';
+      elPc.style.display = 'block';
+  }
+  console.log(CK_DEVICE);
 };
 fnRwd(winWidth);
 
 window.addEventListener('resize', e=>{
   const winReWidth = window.innerWidth;
   //location.reload(); //새로고침
-  console.log( winReWidth );
+  // console.log( winReWidth );
   fnRwd(winReWidth);
 });
