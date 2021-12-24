5f9a1b3776dc8d9b0228ef36a602b233034bd82f
xido <erivery2@gmail.com>
2021-12-24 13:10:24 +0900

    [add] responsive js :ab: 기존 구형브라우저 환경에서 크기가 변할 경우 수행하는 반응형 js기능

--- c_code/b_step_03/dist/src/d_12_rwd.js
+++ c_code/b_step_03/dist/src/d_12_rwd.js
@@ -1,9 +1,9 @@
 // d_12_rwd.js
 
 // @media 화면타입 and (max-width:768px){}
-
+const elWrap   = document.querySelector('#wrap');
 const elMobile = document.querySelector('.mobile');
-const elPc = document.querySelector('.pc');
+const elPc     = document.querySelector('.pc');
 
 // ----------------------------------------------
 const tab = 768; // 모든브라우저 사용가능
@@ -17,9 +17,6 @@ const dataDevice = [
 ];
 
 let CK_DEVICE;
-
-// const tabDevice = '(max-width:768px)'; // 구형X
-
 let winWidth = window.innerWidth; // 브라우저의 가로크기
 // console.log( winWidth );
 
@@ -27,17 +24,18 @@ const fnSwitchView = (type)=>{
   switch(type){
     case dataDevice[0].type:
     case dataDevice[1].type:
-      elMobile.style.display = 'block';
-      elPc.style.display = 'none';
+      elWrap.append(elMobile);
+      elPc.remove()
       break;
     default:
-      elMobile.style.display = 'none';
-      elPc.style.display = 'block';
+      elMobile.remove();
+      elWrap.append(elPc);
   }
 };
 
 const fnRwd = (deviceType)=>{
-  let beforeDevice = CK_DEVICE;
+  let beforeDevice = CK_DEVICE; // 새로 체크되는 크기를 파악하기 위해 이전값을 별도로 저장
+
   if(deviceType <= dataDevice[0].size){
     CK_DEVICE = dataDevice[0].type;
   }else if(deviceType <= dataDevice[1].size){
@@ -48,8 +46,11 @@ const fnRwd = (deviceType)=>{
     CK_DEVICE = dataDevice[3].type;
   }
 // --------------------
+// 사이즈가 변경되었을 때 이전에 있던 크기타입과, 현재 반영된 크기타입이 다른지 체크
+// 320px (mobile), 480px (mobile)  이미 모바일환경인데 다시 모바일환경으로 재 세팅??
+
   if(beforeDevice !== CK_DEVICE){
-    console.log(CK_DEVICE);
+    // console.log(CK_DEVICE);
     fnSwitchView(CK_DEVICE);
   }
 };
@@ -59,3 +60,8 @@ window.addEventListener('resize', e=>{
   const winReWidth = window.innerWidth;
   fnRwd(winReWidth);
 });
+
+// 현재의 브라우저 사이즈가 변경되었을 때!!!
+// 내가 현재 사용하는 디바이스 환경이 변화되었는가 아닌가를 판단
+// 기존 디바이스환경과 다른 환경이면 새로운 환경으로 재 세팅해라!
+// window.matchMedia([css조건의 내용와 동일하게 작성]).matches  -> true/false
