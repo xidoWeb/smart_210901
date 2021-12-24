2f78d677d0d78d2762e544b6b84c97318972712a
xido <erivery2@gmail.com>
2021-12-24 12:42:43 +0900

    [add] :ab: 반응형 처리를 위한 js코드 타입 체크 및 중복기능시 처리

--- c_code/b_step_03/dist/src/d_12_rwd.js
+++ c_code/b_step_03/dist/src/d_12_rwd.js
@@ -22,18 +22,9 @@ let CK_DEVICE;
 
 let winWidth = window.innerWidth; // 브라우저의 가로크기
 // console.log( winWidth );
-const fnRwd = (deviceType)=>{
-  if(deviceType <= dataDevice[0].size){
-    CK_DEVICE = dataDevice[0].type;
-  }else if(deviceType > dataDevice[0].size && deviceType <= dataDevice[1].size){
-    CK_DEVICE = dataDevice[1].type;
-  }else if(deviceType > dataDevice[1].size && deviceType <= dataDevice[2].size){
-    CK_DEVICE = dataDevice[2].type;
-  }else{
-    CK_DEVICE = dataDevice[3].type;
-  }
-// --------------------
-  switch(CK_DEVICE){
+
+const fnSwitchView = (type)=>{
+  switch(type){
     case dataDevice[0].type:
     case dataDevice[1].type:
       elMobile.style.display = 'block';
@@ -43,13 +34,28 @@ const fnRwd = (deviceType)=>{
       elMobile.style.display = 'none';
       elPc.style.display = 'block';
   }
-  console.log(CK_DEVICE);
+};
+
+const fnRwd = (deviceType)=>{
+  let beforeDevice = CK_DEVICE;
+  if(deviceType <= dataDevice[0].size){
+    CK_DEVICE = dataDevice[0].type;
+  }else if(deviceType <= dataDevice[1].size){
+    CK_DEVICE = dataDevice[1].type;
+  }else if(deviceType <= dataDevice[2].size){
+    CK_DEVICE = dataDevice[2].type;
+  }else{
+    CK_DEVICE = dataDevice[3].type;
+  }
+// --------------------
+  if(beforeDevice !== CK_DEVICE){
+    console.log(CK_DEVICE);
+    fnSwitchView(CK_DEVICE);
+  }
 };
 fnRwd(winWidth);
 
 window.addEventListener('resize', e=>{
   const winReWidth = window.innerWidth;
-  //location.reload(); //새로고침
-  // console.log( winReWidth );
   fnRwd(winReWidth);
 });
