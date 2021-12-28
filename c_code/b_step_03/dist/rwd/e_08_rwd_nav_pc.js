7bc74610129ec52f13bc300367954bf05cc7b833
xido <erivery2@gmail.com>
2021-12-28 11:07:48 +0900

    [add] responsive web js :ab: pc기반 js기능구현

--- c_code/b_step_03/dist/rwd/e_08_rwd_nav_pc.js
+++ c_code/b_step_03/dist/rwd/e_08_rwd_nav_pc.js
@@ -12,11 +12,38 @@ const elNavBox = document.querySelector('#navBox');
 const elGnb    = elNavBox.querySelector('.gnb');
 const elGnbLi  = elGnb.children;
 const elGnbLiArr = [...elGnbLi];
-console.log(elGnbLiArr);
+
+const OPTION_NAME = 'on';
+
 
 // 이벤트 
-elGnbLiArr.forEach((el,idx)=>{
+elGnbLiArr.forEach((el, idx)=>{
   el.addEventListener('mouseenter', (e)=>{
-    console.log(e.target.tagName);
+    const elTarget = e.target;
+    elTarget.classList.add(OPTION_NAME);
   });
+
+  el.addEventListener('mouseleave', (e)=>{
+    const elTarget = e.target;
+    elTarget.classList.remove(OPTION_NAME);
+  });
+
+  const elLink = el.querySelector('.gnb_title').children[0];
+  elLink.addEventListener('focus', (e)=>{
+    // const elTarget = e.target.parentNode.parentNode;
+    // console.log(elTarget); // a를 기준으로 부모의 부모   
+
+    for(let i=0; i<elGnbLiArr.length; i++){
+      if(i !== idx){
+        elGnbLiArr[i].classList.remove(OPTION_NAME);
+      }else{
+        // elTarget.classList.add(OPTION_NAME);
+        elGnbLiArr[i].classList.add(OPTION_NAME);
+      }
+    }
+  });
+  // .sub_list의 마직막 요소의 a에 focusout 또는 blur처리시 li에 on제거
 });
+
+
+
