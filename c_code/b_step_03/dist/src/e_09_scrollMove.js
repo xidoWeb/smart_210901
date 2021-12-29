321567452bf86cd75737ab89e20b449c5101fbcc
xido <erivery2@gmail.com>
2021-12-29 11:02:21 +0900

    [add] js-scroll :ab: javascript 스크롤 이벤트 핸들러

--- c_code/b_step_03/dist/src/e_09_scrollMove.js
+++ c_code/b_step_03/dist/src/e_09_scrollMove.js
@@ -1,7 +1,16 @@
 // e_09_scrollMove.js
 
+const headBox = document.querySelector('#headBox');
+const OPTION_FIX = 'fix';
+
+const offsetCheck = headBox.offsetTop;
+console.log( offsetCheck );
 
 
 window.addEventListener('scroll', e=>{
-  console.log( e );
+  const target = parseInt(e.currentTarget.scrollY);
+  console.log('scroll:'+target, 'offset:'+offsetCheck );
+  const headClass = headBox.classList;
+  // 스크롤값이 현재 offset값보다 커지면 .fix 추가
+  (target >= offsetCheck) ? headClass.add(OPTION_FIX) : headClass.remove(OPTION_FIX);
 });
