a150e5ee8a13e6e32f9b581c8bc07b911738b3d7
xido <erivery2@gmail.com>
2021-12-29 11:53:01 +0900

    [add] :ab: js-scroll사용전 세팅

--- c_code/b_step_03/dist/src/e_09_scrollMove.js
+++ c_code/b_step_03/dist/src/e_09_scrollMove.js
@@ -4,7 +4,8 @@ const headBox = document.querySelector('#headBox');
 const OPTION_FIX = 'fix';
 
 const offsetCheck = headBox.offsetTop;
-console.log( offsetCheck );
+// console.log( offsetCheck );
+
 
 
 window.addEventListener('scroll', e=>{
@@ -14,3 +15,13 @@ window.addEventListener('scroll', e=>{
   // 스크롤값이 현재 offset값보다 커지면 .fix 추가
   (target >= offsetCheck) ? headClass.add(OPTION_FIX) : headClass.remove(OPTION_FIX);
 });
+
+/**
+ * scrollTop : 선택요소(element) 기준 스크롤의 움직임의 위치 값 
+ * scrollY   : 브라우저(window) 기준 스크롤의 움직임의 위치 값
+ * offsetTop : 선택자의 상단에서부터 떨어져있는 위치(스크롤바기준 0부터)
+ * scrollTo
+ * scrollBy
+ * 
+ * mousewheel / DOMMouseScroll
+*/
