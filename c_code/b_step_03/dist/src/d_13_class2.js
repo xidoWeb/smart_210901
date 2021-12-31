bcdec114f16ca231230676888e8693ced8e6a9cc
xido <erivery2@gmail.com>
2021-12-31 11:46:07 +0900

    [add] :ab:  비밀번호 속성변경으로 내용확인하기

--- c_code/b_step_03/dist/src/d_13_class2.js
+++ c_code/b_step_03/dist/src/d_13_class2.js
@@ -5,7 +5,9 @@ const userId       = document.querySelector('#userId');
 const userPw       = document.querySelector('#userPw');
 const userRemember = document.querySelector('#userRemember');
 const submitBtn    = document.querySelector('button[type="submit"]');
-//-------------------------------------------------------------------
+const pwViewBtn    = document.querySelector('.pw_view');
+
+//---------------------------------------------------
 
 class CheckMember {
   constructor (id, pw, option){
@@ -17,6 +19,24 @@ class CheckMember {
 
 //-------------------------------------------------------------------
 // 이벤트
+
+// 비밀번호확인버튼 클릭시 비번 확인
+pwViewBtn.addEventListener('mousedown', (e)=>{
+  e.preventDefault();
+  // 왼버튼 클릭 확인 (e.button에서 0번)
+  if( e.button === 0 ){ userPw.type = 'text'; }
+}); 
+pwViewBtn.addEventListener('mouseup', (e)=>{
+  e.preventDefault();
+  userPw.type = 'password'
+}); 
+pwViewBtn.addEventListener('mouseleave', (e)=>{
+  e.preventDefault();
+  userPw.type = 'password'
+});
+
+
+// data 전송시 객체로 처리
 submitBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   const sendData = new CheckMember(userId.value, userPw.value, userRemember.checked);
