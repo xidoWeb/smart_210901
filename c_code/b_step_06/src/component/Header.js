3e691762453caecf35c9f523ba80a3f7f5aa9537
xido <erivery2@gmail.com>
2022-01-03 13:17:39 +0900

    [add] :ab: react map메소드 사용예시1

--- c_code/b_step_06/src/component/Header.js
+++ c_code/b_step_06/src/component/Header.js
@@ -6,14 +6,23 @@ export default function Header({site, url}){
   // const url = "http://naver.com";
 
   const h1Style = { color:'#05f', textTransform: 'uppercase' };
+  const navLists = ['about','product','gallery','notice'];
 
   return (
       <header id="headBox">
         <h1 style={h1Style}>
-          <a 
-            href={url} 
-            target="_blank">{site}</a>
+          <a href={url} target="_blank">{site}</a>
           </h1>
+          <nav className="gnb">
+            <h2>navigation</h2>
+            <ul>
+              {/* <li><a href="/about">about</a></li>
+              <li><a href="/product">product</a></li>
+              <li><a href="/gallery">gallery</a></li>
+              <li><a href="/notice">notice</a></li> */}
+              { navLists.map( (li, idx) => <li key={idx}>{li}</li> ) }
+            </ul>
+          </nav>
       </header>
   )
 }
