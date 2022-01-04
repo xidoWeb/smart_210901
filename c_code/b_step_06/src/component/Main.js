b336782fb33549cb134bbb190f1aebc4ecf2a48e
xido <erivery2@gmail.com>
2022-01-04 13:21:33 +0900

    [add] :ab: useEffet 기본개념

--- c_code/b_step_06/src/component/Main.js
+++ c_code/b_step_06/src/component/Main.js
@@ -1,11 +1,12 @@
 import ConBox from './ConBox';
+import Review from './Review.jsx';
 
 export default function Main() {
   return (
     <main className="container">
         <section id="viewBox">section</section>
         <ConBox />
-        
+        <Review />        
     </main>
   )
 }
---
+++ c_code/b_step_06/src/component/Review.jsx
@@ -0,0 +1,30 @@
+import React,{useState, useEffect} from 'react';
+import '../style/Review.scss';
+import axios from 'axios';
+
+export default function Review() {
+  const [review, setReview] = useState('글자를 입력하세요.');
+  const fnReviewChange = (e)=>{
+    setReview( e.target.value );
+  };
+  
+  useEffect( ()=>{
+    console.log( review );
+  }, [review])
+
+  // useState()  : 하나의 내용에서 변경될 내용을 적용하기 위한 API
+  // useEffect() : 상태체크(변화시 어떠한 기능을 확인체크), fetching
+
+  return (
+    <div className="review_area">
+      <form method="POST" action="#">
+        <fieldset>
+          <legend>리뷰 작성</legend>
+          <input id="reviewText" name="review__Text" 
+          onChange={fnReviewChange} />
+          <p>{review}</p>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
