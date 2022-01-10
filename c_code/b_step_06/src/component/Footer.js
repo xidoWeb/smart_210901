60612bce21ed808f235ce598b5ddb0b8eb8b3279
xido <erivery2@gmail.com>
2022-01-10 12:17:05 +0900

    [add] :ab: NavLink이용하여 class기능 첨부

--- c_code/b_step_06/src/component/Footer.js
+++ c_code/b_step_06/src/component/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import '../style/Footer.scss';
 
 export default function Footer(props){
+
+  const fnAction = ({isActive})=> isActive ? "on" : '' ;
+
   return (
     <footer id="footBox">
       <h2>{props.heading} 정보</h2>
       <div className="company_info">
         <ul>
-          <li><Link to="/manual">이용약관</Link></li>
-          <li><Link to="/person">개인정보처리</Link></li>
-          <li><Link to="/rule">회사방침</Link></li>
-          <li><Link to="/etc">기타</Link></li>
+          <li><NavLink className={fnAction} to="/manual">이용약관</NavLink></li>
+          <li><NavLink className={fnAction} to="/person">개인정보처리</NavLink></li>
+          <li><NavLink className={fnAction} to="/rule">회사방침</NavLink></li>
+          <li><NavLink className={fnAction} to="/etc">기타</NavLink></li>
         </ul>
       </div>
     </footer>
