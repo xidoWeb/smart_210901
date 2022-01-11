f17bb66e37ddf1a500f77963e4207168478e225c
xido <erivery2@gmail.com>
2022-01-11 16:21:19 +0900

    [add] react :ab: 객체불러오기 및 useState

--- c_code/b_step_06/src/component/Product.js
+++ c_code/b_step_06/src/component/Product.js
@@ -1,32 +1,60 @@
 // Product.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/Product.scss';
 
 export default function Product() {
+  // const num = 0, const setNum = (i)=>{ num = i }
+  const [num, setNum] = useState(0);
   // ------------------------------------------
-  const prCon = {
-    "pageTitle" : "cofee",
-    "pageContent" : "coffee 내용에 따른 설명",
-    "products": [
-      { "product":"espresso"   , "narr": "espresso 설명" },
-      { "product":"americano"  , "narr": "아메리카노 설명" },
-      { "product":"latte"      , "narr": "latte 설명" },
-      { "product":"cafe mocca" , "narr": "cafe mocca 설명" },
-      { "product":"apogato"    , "narr": "apogato 설명" }
-    ]
-  };
+  const menuData = [
+    {
+      "pageTitle" : "cofee",
+      "pageContent" : "coffee 내용에 따른 설명",
+      "products": [
+        { "product":"espresso"   , "narr": "espresso 설명" },
+        { "product":"americano"  , "narr": "아메리카노 설명" },
+        { "product":"latte"      , "narr": "latte 설명" },
+        { "product":"cafe mocca" , "narr": "cafe mocca 설명" },
+        { "product":"apogato"    , "narr": "apogato 설명" }
+      ]
+    },
+    {
+      "pageTitle" : "drink",
+      "pageContent" : "drink 내용에 따른 설명",
+      "products": [
+        { "product":"orange" , "narr": "orange 설명" },
+        { "product":"포도"   , "narr": "포도 설명" },
+        { "product":"kiwi"   , "narr": "kiwi 설명" },
+        { "product":"banana" , "narr": "banana 설명" }
+      ]                      
+    }
+  ]
+
+  let prCon = menuData[num];
+  const fnTab = (e, i)=>{
+    e.preventDefault();
+    setNum(i);
+    prCon = menuData[num];
+  }
 
   // ------------------------------------------
   return (
     <div className='product_area'>
-      <h2>{prCon.pageTitle}</h2>
-      <p>{prCon.pageContent}</p>
-      <ul>
-        { prCon.products.map( (pr, idx) => (
-          <li key={idx}><dl><dt>{pr.product}</dt><dd>{pr.narr}</dd></dl></li>
-          ) )}
-      </ul>
+      <div className="btn_part">
+        <button type="button" onClick={(e)=> fnTab(e, 0) }>Coffee</button>
+        <button type="button" onClick={(e)=> fnTab(e, 1) }>Drink</button>
+      </div>
+
+      <div className="content_part">
+        <h2>{prCon.pageTitle}</h2>
+        <p>{prCon.pageContent}</p>
+        <ul>
+          { prCon.products.map( (pr, idx) => (
+            <li key={idx}><dl><dt>{pr.product}</dt><dd>{pr.narr}</dd></dl></li>
+            ) )}
+        </ul>
+      </div>
     </div>
   )}
 
