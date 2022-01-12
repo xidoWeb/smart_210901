eec199c6ce132c5a1bbaaab92245552e12b90c4a
xido <erivery2@gmail.com>
2022-01-12 12:16:55 +0900

    [modify] react footer, 404페이지 간단한 디자인

--- c_code/b_step_06/src/component/NotFound.js
+++ c_code/b_step_06/src/component/NotFound.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className='blank_wrap'>
       <h2>NotFound</h2>
       <p>현재페이지는 없는 페이지입니다.!</p>
     </div>
