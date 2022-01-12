60c1a92125156f76bc1487c3339257b530f1fa6e
xido <erivery2@gmail.com>
2022-01-12 10:18:56 +0900

    [add] react :ab: jsondata 불러와서 탭메뉴 구성 정리

--- c_code/b_step_06/src/component/Product.js
+++ c_code/b_step_06/src/component/Product.js
@@ -6,46 +6,32 @@ import ProductContent from './ProductContent';
 import '../style/Product.scss';
 
 export default function Product() {
-  /*
-  // const num = 0, const setNum = (i)=>{ num = i }
   const [num, setNum] = useState(0);
-  // ------------------------------------------
-  // const [menuData, setMenuData] = useState([]);
+  const [menuData, setMenuData] = useState([]);
 
   useEffect(()=>{
     axios.get('./data/menuData.json')
     .then(res=>  setMenuData(res.data) )
   }, [])
-
-  console.log( menuData );
-
-  // let prCon = menuData[num];
   const fnTab = (e, i)=>{
     e.preventDefault();
     setNum(i);
-    //  const prCon = menuData[num];
   }
-  */
-  const [num, setNum] = useState(0);
-  const [menuData, setMenuData] = useState([]);
-  useEffect(()=>{
-    axios.get('./data/menuData.json')
-    .then(res=>  setMenuData(res.data) )
-  }, [])
 
-  const code = menuData.filter( (data, index) => index === num );
-  
+  const code = menuData.filter( (data, index) => index === num );  
   // ------------------------------------------
   return (
     <div className='product_area'>
       <div className="btn_part">
         {/* <button type="button" onClick={(e)=> fnTab(e, 0) }>Coffee</button>
         <button type="button" onClick={(e)=> fnTab(e, 1) }>Drink</button> */}
+        { menuData.map((data,idx)=> 
+            <button 
+              key={data.pageTitle} type="button" onClick={(e)=> fnTab(e, idx)}>
+                {data.pageTitle}
+            </button>) }
       </div>
-      {/* {menuData.map( data => <p key={data.pageTitle}>{data.pageTitle}</p> )} */}      
       {code.map( (data,idx) => <ProductContent key={idx} data={data} /> )}
-      
-      {/* <ProductContent data={code} /> */}
     </div>
   )}
 
--- c_code/b_step_06/src/component/ProductContent.js
+++ c_code/b_step_06/src/component/ProductContent.js
@@ -4,23 +4,16 @@ import React,{useState, useEffect} from 'react';
 import ProductLi from './ProductLi'
 
 export default function ProductContent({data}) {
-  // const prCon = data;
-  const [list, setList] = useState([]);
-  const [menu, setMenu] = useState([]);
-  useEffect(()=>{
-    setList(data);
-    setMenu(data.products);
-  }, [])
-  console.log( menu )
-
+  const list = data;
   return (
     <div className="content_part">      
       <h2>{list.pageTitle}</h2>
       <p>{list.pageContent}</p>
+
       <ul>
-        {/* { list.products.map( (pr, idx) => <li key={idx}><dl><dt>{pr.product}</dt><dd>{pr.narr}</dd></dl></li> ) } */}
-        { menu.map( (pr, idx) => <ProductLi key={idx} data={pr} /> ) }
+        { list.products.map((card,idx)=> <ProductLi key={idx} data={card}/> )}
       </ul>
     </div>
   )
 }
+
