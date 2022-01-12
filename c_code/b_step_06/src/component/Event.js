bb648bbb6c0a11fa28cb50faa2e09dfe9ec1f108
xido <erivery2@gmail.com>
2022-01-12 13:01:16 +0900

    [add] react :ab: data불러와서 이벤트페이지 구성 (10개만보이기)

--- c_code/b_step_06/src/component/Event.js
+++ c_code/b_step_06/src/component/Event.js
@@ -1,24 +1,30 @@
 // Event.js
 
-import React from 'react';
+import React,  { useEffect, useState }  from 'react';
 import { MdExpandMore } from 'react-icons/md';
-import {Link} from 'react-router-dom';
+
+import axios from 'axios';
 import '../style/Event.scss';
+import EventLi from './EventLi';
 
 export default function Event() {
+
+  const [dataList, setDataList] = useState([]);
+
+  // data 불러오기
+  const dataUrl = './data/eventList.json';
+
+  useEffect(()=>{
+    axios.get(dataUrl).then(res => setDataList(res.data) )
+  }, [])
+
+  const viewData = dataList.filter((data,i)=> i < 10 );
+
   return (
     <article className='event_area'>
       <h2>Event</h2>
       <ul>
-        <li>
-          <div className='num'>1</div>
-          <div className='img_detail'>이미지</div>
-          <dl>
-            <dt>title</dt>
-            <dd>content</dd>
-            <dd className='link_btn'><Link to="#">자세히보기</Link></dd>
-          </dl>
-        </li>
+        { viewData.map( (data,idx)=> <EventLi  key={idx} data={data} />  )}        
       </ul>
 
       <div className='more_btn'>
--- c_code/b_step_06/src/component/EventLi.js
+++ c_code/b_step_06/src/component/EventLi.js
@@ -1,1 +1,20 @@
 // EventLi.js
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function EventLi({data}) {
+  return (
+    <li>
+      <div className='num'>{data.id}</div>
+      <div className='img_detail' style={ {backgroundImage:`url(${data.image})`} }>
+        <span className='blind'>{data.title}</span>
+      </div>
+      <dl>
+        <dt>{data.title}</dt>
+        <dd>날짜 : {data.date}</dd>
+        <dd>{data.content}</dd>
+        <dd className='link_btn'><Link to="#">자세히보기</Link></dd>
+      </dl>
+    </li>
+  )
+}
