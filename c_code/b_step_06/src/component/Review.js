e0d76fe4ae7fc77620f4b1746b6a1ec457b968c0
xido <erivery2@gmail.com>
2022-01-17 17:20:02 +0900

    [add] :ab: styled-components 사용법

--- c_code/b_step_06/src/component/Review.js
+++ c_code/b_step_06/src/component/Review.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 // import axios from 'axios';
+import Styled from 'styled-components';
 import '../style/Review.scss';
 
 
@@ -17,23 +18,42 @@ export default function Review() {
   //   // const response = await axios.get('./data/dataSample.json');
   //   // const dataResult = await response.data;
   //   // console.log(dataResult);
-  // }, [])
-  
-  
+  // }, [])  
   
   // useState()  : 하나의 내용에서 변경될 내용을 적용하기 위한 API
   // useEffect() : 상태체크(변화시 어떠한 기능을 확인체크), fetching
 
   return (
     <div className="review_area">
-      <form method="POST" action="#">
+      {/* <form method="POST" action="#">
         <fieldset>
           <legend>리뷰 작성</legend>
           <input id="reviewText" name="review__Text" 
           onChange={fnReviewChange} />
           <p>{review}</p>
         </fieldset>
-      </form>
+      </form> */}
+
+      <FormSet method="POST" action="#">
+        <Field>
+          <Legend>리뷰 작성</Legend>
+          <input id="reviewText" name="review__Text" 
+          onChange={fnReviewChange} />
+          <p>{review}</p>
+        </Field>
+      </FormSet>
     </div>
   )
 }// Review()
+
+
+const FormSet = Styled.form`
+  width:100%; height:auto; min-height:500px; padding:1rem 2rem; margin:1rem auto; border:1px solid #333;
+`
+const Field = Styled.fieldset`
+  width:100%; height:auto; min-height:500px; background-color:#fff;
+`
+const Legend = Styled.legend`
+  width:1px; height:1px; position:absolute; clip:rect(0,0,0,0); overflow:hidden; z-index:-1;
+`
+
