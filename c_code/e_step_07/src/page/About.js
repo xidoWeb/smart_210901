6077c71e6fc5bf21e44abca72a7ddc09ccbdca94
xido <erivery2@gmail.com>
2022-01-19 16:24:24 +0900

    [add] react :ab: About페이지 card list 처리

--- c_code/e_step_07/src/page/About.js
+++ c_code/e_step_07/src/page/About.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import '../style/About.scss';
+
 
 function About() {
+  const mockData = './data/MOCK_DATA.json';
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(()=>{
+    (async ()=>{
+      const rel = await axios.get(mockData);
+      setCardList(rel.data);
+    })()
+  }); 
+
+  const filterData = cardList.filter( (card,idx) => idx < 20 );
+
   return (
-    <div>
-      About page
+    <div className='page_wrap'>
+      <h2>About page</h2>
+      <ul className='card_wrap'>
+        {filterData.map( (card, idx) => 
+          <li key={idx}>{card.first_name}</li> 
+        )}
+      </ul>
+
     </div>
   )
 }
